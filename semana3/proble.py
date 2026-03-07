5d2832972c725dccd5c10d1eea01654b9424eb7a
izquierdo50 <sairoluisi@gmail.com>
2026-03-06 21:38:45 -0500

    proble.py

---
+++ semana3/proble.py
@@ -0,0 +1,8 @@
+#base y latura del triangulo 
+base=int(input(" base del triangulo "))
+altura= int(input("altura  del triangulo"))
+area=(base*altura)/2
+
+print ("ela are es mayor que 100",area)
+print("el area del trianguo es mayor que 100",area>100)
+#area=("base",*"altura"/2)
