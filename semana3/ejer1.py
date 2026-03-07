d87b4d26bc4b87b2d7da7419d7f16e9a53111366
izquierdo50 <sairoluisi@gmail.com>
2026-03-06 21:10:53 -0500

    primer programa

---
+++ semana3/ejer1.py
@@ -0,0 +1,24 @@
+ esto un dato float
+a=7.5
+
+#este unn dato de tipo entero
+b=20
+print(a+b)
+print("la suma es :",a+b)
+print(a-b)
+print("la resta es :",a+b)
+print(a*b)
+print("la multiplicacion es :",a+b)
+print(a/b)
+print("la divicion es :",a+b)
+print(a//b)
+print("la didvionentera es :",a+b)
+
+#resuido dela  divicion
+print("el resueltado resuido :",a%b)
+print(a%b)
+
+#comoparaciones
+print(a<b)
+print(a>b)
+print(a==b)
