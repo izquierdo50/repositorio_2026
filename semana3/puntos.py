8e83f912c62832e2f1e9ec6dec286ce06470d6b4
izquierdo50 <sairoluisi@gmail.com>
2026-03-06 21:14:43 -0500

    puntos.py
    Implement a simple algorithm to compare two scores.

---
+++ semana3/puntos.py
@@ -0,0 +1,8 @@
+#algoritmo
+#pedir el primer puntaje
+#comparar puntaje 1 con el puntaje 2
+
+puntaje1=int (input("primerpuntaje"))
+puntaje2=int(input("segundopuntaje"))
+
+print(puntaje1>puntaje2)
